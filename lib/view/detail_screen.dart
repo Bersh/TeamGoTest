552757265a4643e5233973023ed45099435c717a
Ilya Bershadskyi <ibersh20@gmail.com>
2019-08-04 13:52:53 +0200

    Imporved details page

--- lib/view/detail_screen.dart
+++ lib/view/detail_screen.dart
@@ -1,3 +1,4 @@
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -21,12 +22,12 @@ class _DetailsState extends State<DetailScreen> {
 
   _DetailsState(this._event);
 
-  final DateFormat dateFormat = new DateFormat("dd-MM-yyyy hh:mm");
+  final DateFormat _dateFormat = new DateFormat("dd-MM-yyyy hh:mm");
   TextEditingController _nameInputController = new TextEditingController();
   TextEditingController _orgInputController = new TextEditingController();
   TextEditingController _locationInputController = new TextEditingController();
-  TextEditingController _timeInputController =
-      new TextEditingController(); //TODO remove
+  DateTime _dateTime = DateTime.now();
+  final _formKey = GlobalKey<FormState>();
 
   Widget _getDataText(String data) {
     return Expanded(
@@ -55,15 +56,24 @@ class _DetailsState extends State<DetailScreen> {
         ));
   }
 
-  Widget _createInputRow(String title, TextEditingController controller) {
-    return TextField(
+  Widget _createInputRow(
+      AppLocalizations local, String title, TextEditingController controller) {
+    return TextFormField(
       decoration: InputDecoration(hintText: title),
       controller: controller,
+      onEditingComplete: () {
+        _formKey.currentState.validate();
+      },
+      validator: (value) {
+        if (value.isEmpty) {
+          return local.translate("empty_value");
+        }
+        return null;
+      },
     );
   }
 
-  Widget _createSaveButton(
-      BuildContext context, AppLocalizations local, EventsDao dao) {
+  Widget _createSaveButton(AppLocalizations local, EventsDao dao) {
     return Container(
         alignment: Alignment.centerRight,
         margin: new EdgeInsets.only(top: 10.0),
@@ -72,17 +82,19 @@ class _DetailsState extends State<DetailScreen> {
             icon: Icon(Icons.save),
             label: Text(local.translate("save")),
             onPressed: () {
-              dao.insertEvent(Event(
-                  name: _nameInputController.text,
-                  organiser: _orgInputController.text,
-                  location: _locationInputController.text,
-                  time: DateTime.now()));
-              Navigator.pop(context);
+              if (_formKey.currentState.validate()) {
+                dao.insertEvent(Event(
+                    name: _nameInputController.text,
+                    organiser: _orgInputController.text,
+                    location: _locationInputController.text,
+                    time: _dateTime));
+                Navigator.pop(context);
+              }
             }));
   }
 
-  Widget _createDeleteButton(BuildContext context, AppLocalizations local,
-      EventsDao dao, Event event) {
+  Widget _createDeleteButton(
+      AppLocalizations local, EventsDao dao, Event event) {
     return Container(
         alignment: Alignment.centerRight,
         margin: new EdgeInsets.only(top: 10.0),
@@ -96,7 +108,38 @@ class _DetailsState extends State<DetailScreen> {
             }));
   }
 
-  List<Widget> _createDetailsWidgets(BuildContext context,
+  Widget _createDateButton(AppLocalizations local) {
+    return DateTimeField(
+      format: _dateFormat,
+      autovalidate: true,
+      initialValue: _dateTime,
+      decoration: InputDecoration(
+          hintText: local.translate("date_time"), alignLabelWithHint: true),
+      onSaved: (date) => setState(() {
+        _dateTime = date;
+      }),
+      onShowPicker: (context, currentValue) async {
+        final date = await showDatePicker(
+            context: context,
+            firstDate: DateTime.now(),
+            initialDate: currentValue ?? DateTime.now(),
+            lastDate: DateTime(2100));
+        if (date != null) {
+          final time = await showTimePicker(
+            context: context,
+            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+          );
+          return DateTimeField.combine(date, time);
+        } else {
+          return currentValue;
+        }
+      },
+      validator: (date) =>
+          date == null ? local.translate("invalid_date") : null,
+    );
+  }
+
+  List<Widget> _createDetailsWidgets(
       AppLocalizations local, EventsDao dao, Event event) {
     List<Widget> result = [];
     if (event != null) {
@@ -108,18 +151,17 @@ class _DetailsState extends State<DetailScreen> {
       result.add(_createDataRow(
           local.translate("location"), _event.location.toString()));
       result.add(_createDataRow(
-          local.translate("time"), dateFormat.format(_event.time)));
-      result.add(_createDeleteButton(context, local, dao, _event));
+          local.translate("date_time"), _dateFormat.format(_event.time)));
+      result.add(_createDeleteButton(local, dao, _event));
     } else {
-      result
-          .add(_createInputRow(local.translate("name"), _nameInputController));
-      result.add(
-          _createInputRow(local.translate("organizer"), _orgInputController));
       result.add(_createInputRow(
-          local.translate("location"), _locationInputController));
-      result
-          .add(_createInputRow(local.translate("time"), _timeInputController));
-      result.add(_createSaveButton(context, local, dao));
+          local, local.translate("name"), _nameInputController));
+      result.add(_createInputRow(
+          local, local.translate("organizer"), _orgInputController));
+      result.add(_createInputRow(
+          local, local.translate("location"), _locationInputController));
+      result.add(_createDateButton(local));
+      result.add(_createSaveButton(local, dao));
     }
     return result;
   }
@@ -129,17 +171,20 @@ class _DetailsState extends State<DetailScreen> {
     final EventsDao dao = Provider.of<EventsDao>(context);
     AppLocalizations local = AppLocalizations.of(context);
     return Scaffold(
+        resizeToAvoidBottomPadding: false,
         appBar: AppBar(
           title: Text(
               AppLocalizations.of(context).translate("details_screen_title")),
         ),
         body: Container(
             child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: _createDetailsWidgets(context, local, dao, _event)),
-        )));
+                padding: EdgeInsets.all(20.0),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: _createDetailsWidgets(local, dao, _event)),
+                ))));
   }
 }
